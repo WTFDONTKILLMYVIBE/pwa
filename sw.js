028f5c675d6f3144b48409cabc2c08090cc51683
testmuc007 <test007_muc@outlook.de>
2026-08-10 17:49:37 +0200

    Fix formatting and update asset paths in sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,22 @@
 const CACHE = "v1";
 const ASSETS = [
-  "index.html",
-  "manifest.json",
-  "sw.js",
-  "background.png"
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./background.png"
 ];
 
 self.addEventListener("install", e => {
-  console.assert(e instanceof ExtendableEvent, "Install event invalid");
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(ASSETS))
   );
 });
 
 self.addEventListener("activate", e => {
-  console.assert(e instanceof ExtendableEvent, "Activate event invalid");
   e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", e => {
-  console.assert(e.request instanceof Request, "Fetch request invalid");
   e.respondWith(
     caches.match(e.request).then(resp => resp || fetch(e.request))
   );
